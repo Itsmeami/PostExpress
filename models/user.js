6f879275f0c46c4b7a1f35dbb0c5693dc939f4bb
itsmeami <amandubey4354@gmail.com>
2024-11-24 17:10:23 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(`mongodb+srv://amanjidubey2:aman5820@cluster0.v2csh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+
+const userSchema = mongoose.Schema ({
+    username : String,
+    name : String,
+    age : Number,
+    email : String,
+    password : String,
+    posts : [
+        {type : mongoose.Schema.Types.ObjectId, ref : "post"}
+    ]
+})
+
+module.exports = mongoose.model('user', userSchema); 
